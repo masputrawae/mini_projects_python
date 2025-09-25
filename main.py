e5f3b603f5d85d0b2aea96e6857b0099b84b0fd7
Masputrawae <masputrawae.official@gmail.com>
2025-09-25 19:51:45 +0700

    Adding show_list function and fix-error in created function

--- main.py
+++ main.py
@@ -4,15 +4,44 @@ import json
 import os
 import uuid
 
-data = []
+FILE_NAME = "data.json"
+
+# ===== VALIDATION DATE ===== #
+def valid_date(input_date):
+    try:
+        date_obj = dt.strptime(input_date, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
+# ===== GET DATA JSON ===== #
+def get_data(file_name):
+    if os.path.exists(file_name):
+        try:
+            with open(file_name, "r") as f:
+                return json.load(f)
+        except Exception as e:
+            print(f"Error: {e}")
+    else:
+        return []
+
+# ===== SAVE DATA ===== #
+def save_data(file_name, content):
+    try:
+        with open(file_name, "w") as f:
+            json.dump(content, f, indent=4)
+    except Exception as e:
+        print(f"Error: {e}")
 
 # ===== CREATE ADD TODO_LIST ===== #
 def add_todo(title, content, due_date):
     idstr = str(uuid.uuid4())
+    short_id = idstr[0:8]
     date = dt.now().isoformat()
 
     struc = {
                 "id": idstr, 
+                "short_id": short_id,
                 "title": title, 
                 "content": content, 
                 "create_date": date,
@@ -23,29 +52,44 @@ def add_todo(title, content, due_date):
 
 # ===== CREATE FUNCTION ===== #
 def create():
-    data_struc = []
+    data_struc = get_data(FILE_NAME)
 
     input_title = input("Title >>> ").title()
     input_content = input("Description >>> ")
-    input_due_date = input("Due Date (YYYY-MM-DD) >>> ")
+
+    while True:
+        input_due_date = input("Due Date (YYYY-MM-DD) >>> ")
+        if valid_date(input_due_date):
+            break
+        else:
+            print("Invalid Format, Correct Format (YYYY-MM-DD)")
 
     new_todo = add_todo(input_title, input_content, input_due_date)
-
-    if os.path.exists("data.json"):
-        try:
-            with open("data.json", "r") as f:
-                data_struc = json.load(f)
-        except Exception as e:
-            print(f"Error {e}")
-
     data_struc.append(new_todo)
-    try:
-        with open("data.json", "w") as f:
-            json.dump(data_struc, f, indent=4)
 
-    except Exception as e:
-        print(f"Error {e}")
-        return
+    save_data(FILE_NAME, data_struc)
+
+# ===== SHOW LIST ===== #
+def show_list():
+    data = get_data(FILE_NAME)
+    hr = "\n" + "=" * 40 + "\n"
+    sparator = hr
+    sparator += "ALL TODO".center(40)
+    sparator += hr
+    print(sparator)
+    if data:
+        for i, v in enumerate(data, start=1):
+            print(f'''
+[{i}]. {v["title"]}
+{"-"*40}
+Short Id        :{v["short_id"]}
+Description     :{v["content"]}
+Created         :{v["create_date"]}
+Due Date        :{v["due_date"]}
+Is Done?        :{v["is_done"]}''')
+            print(hr)
+    else:
+        print("No Tasks")
 
 # ===== USER OPTION ===== #
 def user_option():
@@ -64,7 +108,6 @@ def user_option():
             input_user = int(input(menus))
         
             if input_user == 1:
-                print("1")
                 create()
             elif input_user == 2:
                 print("2")
@@ -73,7 +116,7 @@ def user_option():
             elif input_user == 4:
                 print("4")
             elif input_user == 5:
-                print("5")
+                show_list()
             elif input_user == 6:
                 print("6")
                 break
